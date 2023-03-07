640643dd429c6c99a998874d608189a2aba6976f
urvimanvar <urvi.igenerate@gmail.com>
2023-03-07 15:26:19 +0530

    fkhdfgdev

--- app/models/student.model.js
+++ app/models/student.model.js
@@ -109,7 +109,11 @@ Student.removeAll = result => {
   });
 
   Student.innerJoin = result => {
-    var sql = "SELECT Student.designation AS student,Employee.designation FROM employee JOIN student ON student.Employee.designation = employee.id";
+    // var sql = "SELECT Student.designation AS student,Employee.designation FROM employee JOIN student ON student.Employee.designation = employee.id";
+   
+    const query = 'SELECT * ' + 
+                  'FROM `student` ' +
+                  'JOIN `employee` ON `employee`.`id` = `student`.`name`';
     sql.query(sql, function (err, result) {
        if (err) throw err;
       console.log(result);
